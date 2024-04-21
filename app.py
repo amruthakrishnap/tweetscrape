a5923ee7be4d25eda3c4b47f3d9699d817cca8ca
Amruthakrishna-P <31767534+amruthakrishnap@users.noreply.github.com>
2024-04-21 23:28:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, make_response, Response
+from flask import Flask, request, jsonify, render_template, make_response, Response,socketio
 import csv
 import io
 import os
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import logging
 from urllib.parse import quote
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
